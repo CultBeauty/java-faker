941f3c8b0a55b8b9365c2cee2dbc40e9f819b57b
Adam Borocz <boroczadam@gmail.com>
2017-09-06 19:10:34 +0100

    Add Seer support

---
+++ devops/mvn_install_dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# The current directory we're in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker run --name faker-build --rm=true -it \
+    -e _JAVA_OPTIONS="-Duser.timezone=Europe/London" \
+    -v $DIR/..:/srv \
+    -v /var/lib/dev_cache/ivy2:/root/.ivy2 \
+    -v /var/lib/dev_cache/m2:/root/.m2 \
+    -v /var/lib/dev_cache/sbt:/root/.sbt \
+    -w /srv \
+    docker.cultbeauty.org/cultbeauty/scala:8u144-2.12.3 \
+    mvn install
